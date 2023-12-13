8ccf6906b44171bf8394e565807f3b7b2e1a77ec
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-13 17:49:25 -0500

    Update and rename ModHelperData.cs.txt to ModHelperData.cs
    Fixed typo

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,7 +8,7 @@ public static class ModHelperData
 
     public const string Description = "Adds Web Monkey, Assassin Tower Hut, and 5 mini towers to the game";
 
-    public const string Icon = "pack1";
+    public const string Icon = "pack2";
     public const string RepoOwner = "DatJaneDoe";
     public const string RepoName = "DJDTowerPack2;
     public const string DllName = "/TowerMods/DJDTowerPack2.dll";
