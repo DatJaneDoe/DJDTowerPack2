64ae667bd51377d45e50342187219b78bbcbaa74
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-13 17:55:14 -0500

    Update ModHelperData.cs
    Fixed  version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace DatJaneDoeTowerPack;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "40.0";
-    public const string Version = "1.0.2";
+    public const string Version = "1.1.0";
     public const string Name = "DatJaneDoeTowerPack 2";
 
     public const string Description = "Adds Web Monkey, Assassin Tower Hut, and 5 mini towers to the game";
