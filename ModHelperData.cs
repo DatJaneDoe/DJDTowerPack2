7b4e64ef1b0516e5c5c93c5a86ad4faff8c2913f
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2023-12-13 17:56:54 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,9 +7,7 @@ public static class ModHelperData
     public const string Name = "DatJaneDoeTowerPack 2";
 
     public const string Description = "Adds Web Monkey, Assassin Tower Hut, and 5 mini towers to the game";
-
-    public const string Icon = "pack2";
     public const string RepoOwner = "DatJaneDoe";
     public const string RepoName = "DJDTowerPack2;
-    public const string DllName = "/TowerMods/DJDTowerPack2.dll";
+
 }
